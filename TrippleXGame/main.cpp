07c275b9e35818c07f4a17d187ed2063ba5cb708
Lukasz Jastrzebski <luka@lukajast.com>
2024-06-29 16:06:12 +0200

    Changes to the code.

--- TrippleXGame/main.cpp
+++ TrippleXGame/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
 int main() {
+	std::cout << "You entered the vault of the ancient vampire." << std::endl;
+	std::cout << "You need to guess three digits to open the mechanical lock!" << std::endl;
 	return 0;
 }
