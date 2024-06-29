d27901ea541fe35ddb84a08ff2b16f9c4a396705
Lukasz Jastrzebski <luka@lukajast.com>
2024-06-29 16:28:06 +0200

    Variables added.

--- TrippleXGame/main.cpp
+++ TrippleXGame/main.cpp
@@ -3,5 +3,13 @@
 int main() {
 	std::cout << "You entered the vault of the ancient vampire." << std::endl;
 	std::cout << "You need to guess three digits to open the mechanical lock!" << std::endl;
+	
+	int a = 4;
+	int b = 4;
+	int c = 4;
+	int sum = a + b + c;
+	int multp = a * b * c;
+	std::cout << multp;
+
 	return 0;
 }
