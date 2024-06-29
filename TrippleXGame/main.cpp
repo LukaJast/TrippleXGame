f654a85e2d8fda6ff62ddda195ddabc3442b64a7
Lukasz Jastrzebski <luka@lukajast.com>
2024-06-30 00:45:32 +0200

    Couts to the player.

--- TrippleXGame/main.cpp
+++ TrippleXGame/main.cpp
@@ -1,15 +1,22 @@
-#include <iostream>
+#include <iostream> //preprocessor directive 
 
 int main() {
 	std::cout << "You entered the vault of the ancient vampire." << std::endl;
 	std::cout << "You need to guess three digits to open the mechanical lock!" << std::endl;
 	
-	int a = 4;
+	int a = 4; //declaration statement
 	int b = 4;
 	int c = 4;
-	int sum = a + b + c;
-	int multp = a * b * c;
-	std::cout << multp;
 
-	return 0;
+	//a = 66666l;
+
+	int sum = a + b + c; //expression statement
+	int multp = a * b * c;
+
+	std::cout << std::endl;
+	std::cout << "+ There are three numbers in the code" << std::endl;
+	std::cout << "+ They sum add-up to: " << sum << std::endl;
+	std::cout << "+ They multiply to: "<< multp << std::endl;
+
+	return 0; //return statement
 }
