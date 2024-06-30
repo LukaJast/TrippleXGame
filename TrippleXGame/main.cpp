aadb95df5c635986d856349f76c47a49ee2a51f4
Lukasz Jastrzebski <luka@lukajast.com>
2024-06-30 12:11:57 +0200

    Implementaing PrintIndrotuction function.

--- TrippleXGame/main.cpp
+++ TrippleXGame/main.cpp
@@ -1,40 +1,39 @@
 #include <iostream> //preprocessor directive 
 
-void PlayGame() {
+void PrintIntroduction() {
 
 	std::cout << "You entered the vault of the ancient vampire. \n";
 	std::cout << "You need to guess three digits to open the mechanical lock! \n\n";
+
+}
+void PlayGame() {
+
+	PrintIntroduction();
 
 	int CodeA = 4; //declaration statement
 	int CodeB = 4;
 	int CodeC = 4;
 
-	//a = 66666l;
-
 	int CodeSum = CodeA + CodeB + CodeC; //expression statement
 	int CodeProduct = CodeA * CodeB * CodeC;
 
-	std::cout << "+ There are three numbers in the code \n";
+	std::cout << "+ There are three numbers in the code: \n";
 	std::cout << "\n+ They sum add-up to: " << CodeSum;
 	std::cout << "\n+ They multiply to: " << CodeProduct;
 
 	int GuessA, GuessB, GuessC;
 
-	std::cout << "Enter a number: \n";
+	std::cout << "\n\nEnter a number: ";
 	std::cin >> GuessA >> GuessB >> GuessC;
-
-	//std::cout << "You entered: " << GuessA << GuessB << GuessC << std::endl;
 
 	int GuessSum = GuessA + GuessB + GuessC;
 	int GuessProduct = GuessA * GuessB * GuessC;
 
 	if (CodeSum == GuessSum && CodeProduct == GuessProduct) {
-		std::cout << "\n You won!!";
+		std::cout << "\nYou won!!";
+	}	else {
+		std::cout << "\nYou lost!";
 	}
-	else {
-		std::cout << "\n You lost!";
-	}
-
 }
 
 int main() {
