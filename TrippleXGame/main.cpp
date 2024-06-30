117b51c1add4d4367981b2dd7f8dd301d74afa0a
Lukasz Jastrzebski <luka@lukajast.com>
2024-06-30 18:49:46 +0200

    Passing arguments between the functions.

--- TrippleXGame/main.cpp
+++ TrippleXGame/main.cpp
@@ -1,48 +1,65 @@
-#include <iostream> //preprocessor directive 
+﻿#include <iostream> //preprocessor directive 
+#include <windows.h>
 
-void PrintIntroduction() {
+void PrintIntroduction(int Difficulty) {
 
-	std::cout << "\n\nYou entered the vault of the ancient vampire. \n";
-	std::cout << "You need to guess three digits to open the mechanical lock! \n\n";
+	std::cout << "\n\nWchodzisz do " << Difficulty << " sali skarbca \033[1mTeodora\033[0m. \n";
+	std::cout << "Musisz odgadnac trzy cyfry kodu, zeby otworzyc mechaniczny zamek! \n";
 
 }
-bool PlayGame() {
+bool PlayGame(int Difficulty) {
 
-	PrintIntroduction();
+	PrintIntroduction(Difficulty);
 
 	int CodeA = 4; //declaration statement
 	int CodeB = 4;
 	int CodeC = 4;
 
 	int CodeSum = CodeA + CodeB + CodeC; //expression statement
-	int CodeProduct = CodeA * CodeB * CodeC;
+	int CodeProduct = CodeA * CodeB * CodeC; 
 
-	std::cout << "+ There are three numbers in the code: \n";
-	std::cout << "\n+ They sum add-up to: " << CodeSum;
-	std::cout << "\n+ They multiply to: " << CodeProduct;
+	std::cout << "\n+ Sa trzy cyfry w kodzie:";
+	std::cout << "\n+ Ich suma to: " << CodeSum;
+	std::cout << "\n+ Gdy je pomnozyc, wynik bedzie: " << CodeProduct;
 
 	int GuessA, GuessB, GuessC;
 
-	std::cout << "\n\nEnter a number: ";
+	std::cout << "\n\nPodaj cyfry: ";
 	std::cin >> GuessA >> GuessB >> GuessC;
 
 	int GuessSum = GuessA + GuessB + GuessC;
 	int GuessProduct = GuessA * GuessB * GuessC;
 
 	if (CodeSum == GuessSum && CodeProduct == GuessProduct) {
-		std::cout << "\nYou won!!";
+		
+		std::cout << "\n\033[1mWygrales!!\033[0m";
 		return true;
+
 	}	else {
-		std::cout << "\nYou lost!";
+		std::cout << "\n\033[1mPrzegrales!\033[0m";
 		return false;
 	}
 }
 
 int main() {
+	// Set console output to UTF-8
+	SetConsoleOutputCP(CP_UTF8);
+
+	int LevelDifficulty(1);
+	int MaxDifficulty(5);
+
+	//nt LevelDifficulty = 1;
+
 	while (true) {
-		bool bLevelComplete = PlayGame();
+
+		bool bLevelComplete = PlayGame(LevelDifficulty);
 		std::cin.clear();
 		std::cin.ignore();
+		
+		if (bLevelComplete)
+		{
+			++LevelDifficulty; 
+		}
 	}
 
 	return 0; //return statement
