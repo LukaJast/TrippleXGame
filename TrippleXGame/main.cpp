87c030fea269268a7d944da58d8ec8dd7d826f97
Lukasz Jastrzebski <luka@lukajast.com>
2024-06-30 11:47:56 +0200

    Creation of PlayGame function.

--- TrippleXGame/main.cpp
+++ TrippleXGame/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream> //preprocessor directive 
 
-int main() {
+void PlayGame() {
+
 	std::cout << "You entered the vault of the ancient vampire. \n";
 	std::cout << "You need to guess three digits to open the mechanical lock! \n\n";
-	
+
 	int CodeA = 4; //declaration statement
 	int CodeB = 4;
 	int CodeC = 4;
@@ -13,9 +14,9 @@ int main() {
 	int CodeSum = CodeA + CodeB + CodeC; //expression statement
 	int CodeProduct = CodeA * CodeB * CodeC;
 
-	std::cout << "+ There are three numbers in the code \n"
+	std::cout << "+ There are three numbers in the code \n";
 	std::cout << "\n+ They sum add-up to: " << CodeSum;
-	std::cout << "\n+ They multiply to: "<< CodeProduct;
+	std::cout << "\n+ They multiply to: " << CodeProduct;
 
 	int GuessA, GuessB, GuessC;
 
@@ -28,11 +29,16 @@ int main() {
 	int GuessProduct = GuessA * GuessB * GuessC;
 
 	if (CodeSum == GuessSum && CodeProduct == GuessProduct) {
-		std::cout << "You won!! \n";
+		std::cout << "\n You won!!";
 	}
 	else {
-		std::cout << "You lost! \n";
+		std::cout << "\n You lost!";
 	}
+
+}
+
+int main() {
 	
+	PlayGame();
 	return 0; //return statement
 }
